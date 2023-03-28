92db9cb5b9fa56eac3093e2c12a16e74b27283ec
Nika Xucishvili <103494069+NikaXucishvili@users.noreply.github.com>
2023-03-28 23:00:51 +0400

    Card loading function

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let button = document.getElementById('button');
 let loadingScreen = document.getElementById('loading-screen');
-var cards = document.querySelectorAll('.card');
+let image = document.querySelectorAll('.image');
 
 button.addEventListener("click", function(){
     loadingScreen.style.display = 'none';
 });
 
+button.addEventListener("click", function(){
+    for(var i = 0; i < image.length; i++){
+        image[i].style.opacity = "1";
+    }
+})
