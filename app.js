cff92cf8fdde661ecc5ebd58bb916d32f60e49b1
Nika Xucishvili <103494069+NikaXucishvili@users.noreply.github.com>
2023-03-28 12:28:10 +0400

    Added functions

---
+++ app.js
@@ -0,0 +1,8 @@
+let button = document.getElementById('button');
+let loadingScreen = document.getElementById('loading-screen');
+var cards = document.querySelectorAll('.card');
+
+button.addEventListener("click", function(){
+    loadingScreen.style.display = 'none';
+});
+
