01497ec82d8ced4fbd7227066872fa641dacf80b
Nika Xucishvili <103494069+NikaXucishvili@users.noreply.github.com>
2023-03-29 12:22:05 +0400

    CardsFlipFunction

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const button = document.getElementById('button');
 const loadingScreen = document.getElementById('loading-screen');
 const image = document.querySelectorAll('.image');
 
+
 button.addEventListener("click", function(){
     loadingScreen.style.display = 'none';
     for(var i = 0; i < image.length; i++){
@@ -9,3 +10,21 @@ button.addEventListener("click", function(){
     }
 });
 
+button.addEventListener('click', flipCards);
+button.addEventListener('click', widthDone);
+
+function flipCards(){
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.classList.add('flip');
+    });
+}
+
+
+function widthDone(){
+    setTimeout(() => {
+        for(var b = 0; b < image.length; b++){
+            image[b].style.width = "100%";
+        }
+    }, 400);
+}
