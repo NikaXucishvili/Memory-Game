eb5a3432d3ec133e4d2be6681358c07c10559a3d
Nika Xucishvili <103494069+NikaXucishvili@users.noreply.github.com>
2023-03-30 16:14:24 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const button = document.getElementById('button');
 const loadingScreen = document.getElementById('loading-screen');
 const image = document.querySelectorAll('.image');
+const cards = document.querySelectorAll('.card');
+let timeoutId;
 
 // Loading screen function
 
@@ -16,8 +18,40 @@ button.addEventListener("click", function(){
 button.addEventListener('click', flipCards);
 button.addEventListener('click', widthDone);
 
+
+setTimeout(function(){
+    cards.forEach(card => {
+        card.classList.remove('flip');
+        for(var c = 0; c < image.length; c++){
+            image[c].style.opacity = "0";
+        }
+    });
+}, 3000);
+
+// on click card flip function
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        flipCard(card);
+        image.forEach(image => {
+            image.style.opacity = "1";
+        })
+    });
+});
+
+// One card flip function
+
+function flipCard(card){
+    card.classList.add('flip');
+    timeoutId = setTimeout(() => {
+        card.classList.remove('flip');
+    }, 3000);
+}
+
+
+// All cards flip in the start
+
 function flipCards(){
-    const cards = document.querySelectorAll('.card');
     cards.forEach(card => {
         card.classList.add('flip');
     });
